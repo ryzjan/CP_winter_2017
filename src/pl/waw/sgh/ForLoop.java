d54994a070d7d253f8706485486cb3214cbeae8b
jas <ryz.jas@gmail.com>
2017-11-13 22:15:05 +0100

    Adding Integer and String

--- src/pl/waw/sgh/ForLoop.java
+++ src/pl/waw/sgh/ForLoop.java
@@ -4,7 +4,29 @@ public class ForLoop {
     public static void main(String[] args) {
         int c=0;
         c++;
-        System.out.println(c);
+        c=c+1;
+
+        int d;
+        d=c++;
+        //d=++c;
+        //System.out.println(d);
+        int e=--c;
+        System.out.println("c="+c);
+        System.out.println("d="+d);
+
+        //System.out.println();
+
+        String s1="abc";
+        Integer i1=35445;
+        s1=s1+i1;
+
+        String s2=i1.toString();
+        s1=s1.concat(s2);
+
+
+
+
+
 
     }
 }
