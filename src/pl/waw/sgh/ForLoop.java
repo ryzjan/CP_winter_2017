40b0c5a3d6f2935d4e1723257b3674d379102bb3
jas <ryz.jas@gmail.com>
2017-11-13 22:33:30 +0100

    Added if inside for

--- src/pl/waw/sgh/ForLoop.java
+++ src/pl/waw/sgh/ForLoop.java
@@ -20,8 +20,26 @@ public class ForLoop {
         Integer i1=35445;
         s1=s1+i1;
 
-        String s2=i1.toString();
-        s1=s1.concat(s2);
+        //String s2=i1.toString();
+        //s1=s1.concat(s2);
+        System.out.println(s1);
+
+        for (int i=0;i<5;i++){
+            System.out.println("i"+i);
+        }
+
+        /* TODO: What happens here?
+        System.out.println();
+         */
+
+        // if inside for
+        for(int j=5;j>0;j--){
+            //pomija j==4
+            if (j==4) continue;
+            System.out.println("j".concat(new Integer(j).toString()));
+            //System.out.println("j="+j);
+            if(j==2) break;
+        }
 
 
 
