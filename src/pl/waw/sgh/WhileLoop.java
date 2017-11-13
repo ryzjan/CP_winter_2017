fc8dc66e78d729e847d41b66e9218e0dae0e8552
jas <ryz.jas@gmail.com>
2017-11-13 22:58:47 +0100

    While loop, testing different values

---
+++ src/pl/waw/sgh/WhileLoop.java
@@ -0,0 +1,21 @@
+package pl.waw.sgh;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        //int v=12;
+        //int v=3;
+        int v=0;
+        /*
+            while (v<5){
+                System.out.println("v: "+v);
+                v++;
+            }
+        */
+        do {
+            System.out.println("v: "+v);
+            v++;
+        } while(v<5);
+
+
+    }
+}
