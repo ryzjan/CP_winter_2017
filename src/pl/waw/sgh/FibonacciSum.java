1726c4c04b5f7d1149cd25830e2625105b2be26f
jas <ryz.jas@gmail.com>
2017-11-14 22:02:53 +0100

    Function that sums n first Fibonacci numbers

--- src/pl/waw/sgh/FibonacciSum.java
+++ src/pl/waw/sgh/FibonacciSum.java
@@ -1,13 +1,27 @@
 package pl.waw.sgh;
 
 public class FibonacciSum {
+//function:
+    public static int  f_loop(int n){
 
-    public static int  f_loop(int n){}
+        if (n==1||n==2){ //without an extension by initial "0"
+            return 1;
+        }
 
+        int a=1, b=1, c, s=2;
+        for (int i=3;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            s=s+c;
+        }
+        return s;
+    }
 
+//call function:
     public static void main(String[] args) {
-        int n=5;
-        System.out.println(f_loop(n));
+        int n=4;
+        System.out.println("The sum of "+n+" Fibonacci numbers is: "+f_loop(n));
     }
 }
 
