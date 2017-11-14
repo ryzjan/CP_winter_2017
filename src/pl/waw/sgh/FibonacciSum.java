ebdd4acf692cace19e6e25d6ad7a0ca326053b78
jas <ryz.jas@gmail.com>
2017-11-14 21:40:37 +0100

    Beginning of writing a function to sum n numbers of Fibonacci Sequence

---
+++ src/pl/waw/sgh/FibonacciSum.java
@@ -0,0 +1,14 @@
+package pl.waw.sgh;
+
+public class FibonacciSum {
+
+    public static int  f_loop(int n){}
+
+
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(f_loop(n));
+    }
+}
+
+
