069bf53ac2d9198266a5dd4c352af0823c3e1222
jas <ryz.jas@gmail.com>
2017-11-20 22:38:11 +0100

    For loop with arrays

--- src/pl/waw/sgh/Arrays.java
+++ src/pl/waw/sgh/Arrays.java
@@ -1,4 +1,32 @@
 package pl.waw.sgh;
 
 public class Arrays {
+    //function:
+    public static Double sumDoubles(Double[] arr){
+
+        Double sum=0d;
+        for(Double el: arr) {
+            //+= adds el to the sum
+            sum+=el;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Integer[] myIntArr=new Integer[5];
+        Double[] myDbArr=new Double[] {5625.4, 585.78, 45888.11};
+
+        myIntArr[0]=7845;
+        myIntArr[1]=478541;
+        myIntArr[2]=47152;
+
+        Integer sum=0;
+
+        for(int i=0; i<myIntArr.length; i++) {
+            System.out.println("myIntArr, element " +i+" = "+myIntArr[i]);
+        //if breaks here, program shows null elements of myIntArr}
+
+    }
+
+
 }
