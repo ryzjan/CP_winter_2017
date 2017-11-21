816ee63d93338443d40574a1b40925e08130c115
jas <ryz.jas@gmail.com>
2017-11-21 18:30:59 +0100

    Game successfully imported (raw game)

--- src/pl/waw/sgh/TicTacToe.java
+++ src/pl/waw/sgh/TicTacToe.java
@@ -7,35 +7,39 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    static final String FILE_LOC = "/home/jas/game1.txt";
+    static final String FILE_LOC = "/home/jas/tictactoe.txt";
 
     public static void main(String[] args) throws
-            FileNotFoundException {
-        String[][] GameArr = new String[3][3];
-        File file = new File(FILE_LOC);
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNext()) {
-            String myLine = scanner.nextLine();
+            FileNotFoundException{
+        File file=new File(FILE_LOC);
+        String[][] arr2D = new String[3][3];
+        Scanner scanner=new Scanner(file);
+        int i=0;
+        while (scanner.hasNext()){
+
+            String myLine= scanner.nextLine();
             //System.out.println(myLine);
-            String[] elem = myLine.split(",");
-            for (String el : elem) {
-                for (int i = 0; i < GameArr.length; i++) {
-                    for (int j = 0; j < GameArr[i].length; j++) {
-                        GameArr[i][j] = el;
-                    }
-
-
-                }
-                System.out.println("= " + el);
+            String[] elem=myLine.split(",");
+            int j=0;
+            for (String el:elem) {
+                arr2D[i][j]=el;
+                //System.out.println("= " + el);
+                j++;
             }
-
-            }
-        for (int i=0;i<GameArr.length;i++) {
-            // process every cell
-            for (int j=0;j<GameArr[i].length;j++) {
-                System.out.print(GameArr[i][j] + "\t");
+            i++;
+        }
+        for(int k=0;k<arr2D.length;k++){
+            for(int l=0;l<arr2D[k].length;l++) {
+                //not println
+                System.out.print(arr2D[k][l]);
             }
             System.out.println("|");
         }
+        System.out.println("------");
+
     }
 }
+
+
+
+
