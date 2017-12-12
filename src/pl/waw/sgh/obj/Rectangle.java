8ff6feb4bc653efb62e8c35be15339ea0216c9d7
jas <ryz.jas@gmail.com>
2017-12-12 20:45:56 +0100

    Rectangles 1

--- src/pl/waw/sgh/obj/Rectangle.java
+++ src/pl/waw/sgh/obj/Rectangle.java
@@ -3,16 +3,19 @@ package pl.waw.sgh.obj;
 public class Rectangle {
     double sideA;
     double sideB;
-    public Rectangle(){
-        this.sideA=10;
-        this.sideB=15;
-    }
-    public void setParams(double sideA, double sideB){
-        this.sideA=sideA;
-        this.sideB=sideB;
-    }
-    public double calcSurface(){
 
-        return sideA*sideB;
+    public Rectangle() {
+        this.sideA = 10;
+        this.sideB = 15;
+    }
+
+    public void setParams(double sideA, double sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public double calcSurface() {
+
+        return sideA * sideB;
     }
 }
