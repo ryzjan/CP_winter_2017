7ddb64f3dbda60a850c8c54c71c3b8799a2da13e
jas <ryz.jas@gmail.com>
2017-12-12 22:21:54 +0100

    Lists and sets

---
+++ src/pl/waw/sgh/ListsAndSets.java
@@ -0,0 +1,17 @@
+package pl.waw.sgh;
+import java.util.*;
+import java.util.Arrays;
+public class ListsAndSets {
+
+    public static void printCollection(Collection collection) {
+        for(Object setE1:collection){
+            System.out.println(setE1);
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
