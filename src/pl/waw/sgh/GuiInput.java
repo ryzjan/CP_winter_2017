2760f1a2600cd403b051880f78ad5b90618f181a
jas <ryz.jas@gmail.com>
2017-12-12 20:24:34 +0100

    Ex completed

--- src/pl/waw/sgh/GuiInput.java
+++ src/pl/waw/sgh/GuiInput.java
@@ -5,7 +5,14 @@ public class GuiInput {
     public static void main(String[] args) {
 
         int dec=JOptionPane.YES_OPTION;
-
+        while(dec!=JOptionPane.NO_OPTION){
+                String input = JOptionPane.showInputDialog(
+                        null, "Please provide your name: ");
+                JOptionPane.showMessageDialog(null, "Hello " + input);
+                dec= JOptionPane.showConfirmDialog(null, "Do you want to continue?");
+                //Like boolean?
+                JOptionPane.showMessageDialog(null, "Decision is: "+dec);
+        }
 
             }
 }
