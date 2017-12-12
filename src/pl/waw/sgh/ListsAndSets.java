525f46f42bdbed51bfdcf51f253368aa00f657a3
jas <ryz.jas@gmail.com>
2017-12-12 22:40:50 +0100

    Lists and sets - ex completed

--- src/pl/waw/sgh/ListsAndSets.java
+++ src/pl/waw/sgh/ListsAndSets.java
@@ -12,6 +12,27 @@ public class ListsAndSets {
     }
 
     public static void main(String[] args) {
+        List<String>myList=new ArrayList<>();
+        myList.add("abc");
+        myList.add("cde");
+        myList.add(0,"def");
+        myList.add("def");
+        System.out.println("2nd element is: "+myList.get(1));
+        printCollection(myList);
+        //Arrays.
+        Collections.sort(myList);
+        printCollection(myList);
+
+        Set<Integer>mySet=new HashSet<>();
+        mySet.add(713);
+        mySet.add(345);
+        mySet.add(777);
+        mySet.add(123);
+        printCollection(mySet);
+
+        Set<Integer>mySortedSet=new TreeSet<>();
+        mySortedSet.addAll(mySet);
+        printCollection(mySortedSet);
 
     }
 }
