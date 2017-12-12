e38d7ecd9ad878a858777f0f319dd9e7fa35a185
jas <ryz.jas@gmail.com>
2017-12-12 21:33:25 +0100

    Date operations - finish of practice

--- src/pl/waw/sgh/DateOperations.java
+++ src/pl/waw/sgh/DateOperations.java
@@ -1,5 +1,7 @@
 package pl.waw.sgh;
 
+import jdk.nashorn.internal.scripts.JO;
+
 import javax.swing.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,10 +16,35 @@ public class DateOperations {
         //Result: Tue Dec 12 21:17:18 CET 2017
         System.out.println(System.currentTimeMillis());
         //Result: 1513109838812
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String formattedDate = sdf.format(date);
         System.out.println(formattedDate);
         //Result: 2017-12-12 21:17:18
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+        boolean success=false;
+        Date outDate=null;
+                while(!success){
+                    try{
+                        //String inDate = null;
+                        String inDate=JOptionPane.showInputDialog(null, "Please provide the date: ");
+                        outDate=sdf2.parse(inDate);
+
+                        //Add 10 days to the ^^ date
+                        Calendar c=Calendar.getInstance();
+                        c.setTime(outDate);
+                        c.add(Calendar.DATE,10);
+                        outDate=c.getTime();
+                        //outDate=sdf2.parse(inDate);
+                        success=true;
+
+                    } catch (ParseException|NullPointerException e) {
+                        JOptionPane.showMessageDialog(null, "Cannot parse the date provided, please try again!");
+                        e.printStackTrace();
+                    }
+                }
+        JOptionPane.showMessageDialog(null, "Your date: "+sdf.format(outDate));
+        Long stopMillis=System.currentTimeMillis();
+        JOptionPane.showMessageDialog(null, "It took: "+(stopMillis-startMillis)+" ms.");
 
     }
 
