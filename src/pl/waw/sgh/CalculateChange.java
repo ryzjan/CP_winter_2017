d41a282e68915afc9da40af3236ca5fcdf21dc87
jas <ryz.jas@gmail.com>
2017-12-13 22:10:04 +0100

    Code reformatted.

--- src/pl/waw/sgh/CalculateChange.java
+++ src/pl/waw/sgh/CalculateChange.java
@@ -1,4 +1,5 @@
 package pl.waw.sgh;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,7 +21,7 @@ public class CalculateChange {
             if (listOfFiles[i].isFile()) {
 
                 Scanner scan0 = new Scanner(listOfFiles[i]);
-                int l=0;
+                int l = 0;
                 while (scan0.hasNext()) {
                     String row = scan0.nextLine();
                     l++;
@@ -35,17 +36,17 @@ public class CalculateChange {
                     String[] elem = row.split(",");
                     int j = 0;
                     //with exception of headers:
-                    if (k>0){
-                    for (String el : elem) {
+                    if (k > 0) {
+                        for (String el : elem) {
 
-                        if (j == 1) {
-                            arr[k-1][0] = Double.parseDouble(el);
+                            if (j == 1) {
+                                arr[k - 1][0] = Double.parseDouble(el);
+                            }
+                            if (j == 4) {
+                                arr[k - 1][1] = Double.parseDouble(el);
+                            }
+                            j++;
                         }
-                        if (j == 4) {
-                            arr[k-1][1] = Double.parseDouble(el);
-                        }
-                        j++;
-                    }
 
                     }
 
@@ -54,46 +55,38 @@ public class CalculateChange {
                 //Double arrResults[]=new Double[l-1];
 
                 //for (int m=0; m<=(l-2);m++){
-                    //arrResults[m]=((arr[m][1]-arr[m][0])/arr[m][0])*100;
+                //arrResults[m]=((arr[m][1]-arr[m][0])/arr[m][0])*100;
 
-                    Scanner scan1 = new Scanner(listOfFiles[i]);
-                    int m=-1;
-                    while (scan1.hasNext()) {
-                        if(m>-1) {
-                            Double z = ((arr[m][1] - arr[m][0]) / arr[m][0]) * 100;
-                            String row1 = scan1.nextLine();
-                            FileWriter fw = new FileWriter(listOfFiles[i]+"_1", true);
-                            BufferedWriter bw = new BufferedWriter(fw);
-                            bw.newLine();
-                            bw.write(row1 + ", " + Double.toString(z) + "%");
-                            bw.close();
-                            fw.close();
-                        }
-                        if(m==-1) {
-                            String row1 = scan1.nextLine();
-                            FileWriter fw = new FileWriter(listOfFiles[i]+"_1", true);
-                            BufferedWriter bw = new BufferedWriter(fw);
-                            //bw.newLine();
-                            bw.write(row1);
-                            bw.close();
-                            fw.close();
-                        }
-                        m++;
+                Scanner scan1 = new Scanner(listOfFiles[i]);
+                int m = -1;
+                while (scan1.hasNext()) {
+                    if (m > -1) {
+                        Double z = ((arr[m][1] - arr[m][0]) / arr[m][0]) * 100;
+                        String row1 = scan1.nextLine();
+                        FileWriter fw = new FileWriter(listOfFiles[i] + "_1", true);
+                        BufferedWriter bw = new BufferedWriter(fw);
+                        bw.newLine();
+                        bw.write(row1 + ", " + Double.toString(z) + "%");
+                        bw.close();
+                        fw.close();
+                    }
+                    if (m == -1) {
+                        String row1 = scan1.nextLine();
+                        FileWriter fw = new FileWriter(listOfFiles[i] + "_1", true);
+                        BufferedWriter bw = new BufferedWriter(fw);
+                        //bw.newLine();
+                        bw.write(row1);
+                        bw.close();
+                        fw.close();
+                    }
+                    m++;
                     //}
-
-
-
 
 
                 }
 
 
-
-
-
-            }
-
-            else if (listOfFiles[i].isDirectory()) {
+            } else if (listOfFiles[i].isDirectory()) {
                 System.out.println("Directory " + listOfFiles[i].getName());
 
             }
