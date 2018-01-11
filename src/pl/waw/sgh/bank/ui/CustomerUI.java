915c053c5b2f64f4c112f3cd786a139ffaa9e05c
jas <ryz.jas@gmail.com>
2018-01-11 21:43:49 +0100

    Building Customer UI form (only UI)

---
+++ src/pl/waw/sgh/bank/ui/CustomerUI.java
@@ -0,0 +1,11 @@
+package pl.waw.sgh.bank.ui;
+
+import javax.swing.*;
+
+public class CustomerUI {
+    private JPanel panel1;
+    private JTextField textField_FirstName;
+    private JTextField textField_ID;
+    private JTextField textField_LastName;
+    private JTextField textField_Email;
+}
